d9c3b0ef9b8dde8b5ce5d903bab824c984e8372f
华 <sliphua@outlook.com>
2021-11-21 00:39:47 +0800

    Add todo note about browser e2e tests

--- test/browser/e2e.test.ts
+++ test/browser/e2e.test.ts
@@ -1,6 +1,12 @@
 import { test, expect } from '..';
 import onfetch from '../../src';
 
+/**
+ * This test suite is broken as unregister() take effects only after a reload.
+ * TODO Fix this. Some possible solutions:
+ *  1. Unregister immediately when it supports. See [A way to immediately unregister a service worker · Issue #614 · w3c/ServiceWorker](https://github.com/w3c/ServiceWorker/issues/614).
+ *  2. Investigate using an iframe.
+ */
 test.describe('browser e2e', () => {
   test.beforeAll(async () => {
     await onfetch.useServiceWorker();
