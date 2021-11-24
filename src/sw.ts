c1828b61c4cc3a76bfb23725b730da2d27f9e457
华 <sliphua@outlook.com>
2021-11-24 16:56:18 +0800

    Require sw to immediately take effects

--- src/sw.ts
+++ src/sw.ts
@@ -2,6 +2,14 @@ import Worker from './lib/SW/Worker';
 
 declare const globalThis: ServiceWorkerGlobalScope;
 
+// Require immediate effects.
+globalThis.addEventListener('install', (event) => {
+  event.waitUntil(globalThis.skipWaiting());
+});
+globalThis.addEventListener('activate', (event) => {
+  event.waitUntil(globalThis.clients.claim());
+});
+
 const onfetchWorker = new Worker(globalThis);
 
 // eslint-disable-next-line no-void
